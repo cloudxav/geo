cd3f35ebe43155bf119ca0c8291a991d266d3524
cloudxav <sukritilahiri1234@gmail.com>
2022-10-02 23:39:03 +0530

    RS FIRST

---
+++ code.js
@@ -0,0 +1,8 @@
+function init(){
+	var roll = document.getElementsByClassName("input")[0].value;
+	if (roll === "49497"){
+		window.open("https://github.com");
+	}else{
+		window.alert("Invalid Code");
+	}
+}
