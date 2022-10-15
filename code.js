59a19df16b93c9f4de97e94a68a12557355a1767
cloudxav <114873213+cloudxav@users.noreply.github.com>
2022-10-15 23:38:53 +0530

    Update code.js

--- code.js
+++ code.js
@@ -2,17 +2,23 @@ function init(){
 	var roll = document.getElementsByClassName("input")[0].value;
 	var code = document.getElementsByClassName("input")[1].value;
 	if (roll === "21AGEO049497"&&code==="49497"){
-		display();
+		display(document.getElementsByClassName("sem_picker")[0].value);
 	}else{
 		window.alert("Invalid Code");
 	}
-}function display(){
+}
+function display(Strin){
 	document.getElementsByClassName("pdf")[0].style.minHeight = "100%";
 	document.getElementsByClassName("pdf")[0].style.minWidth = "100%";
 	document.getElementsByClassName("pdf")[0].style.width = "100%";
 	document.getElementsByClassName("pdf")[0].style.height = "100%";
-	document.getElementsByClassName("pdf")[0].style.visibility = "visible"
-	document.getElementsByClassName("pdf")[0].src = "https://drive.google.com/file/d/1bLBb0BgC8n25VJNplQOnMbJgYiWJkS51/preview";
+	document.getElementsByClassName("pdf")[0].style.visibility = "visible";
+
+	if(Strin=="SEM-2"){
+		document.getElementsByClassName("pdf")[0].src = "https://drive.google.com/file/d/1bLBb0BgC8n25VJNplQOnMbJgYiWJkS51/preview";
+	}else{
+		document.getElementsByClassName("pdf")[0].src = ""
+	}
 
 	document.getElementsByClassName("main")[0].style.visibility = "hidden";
 	document.getElementsByClassName("main")[0].style.height = "0%";
